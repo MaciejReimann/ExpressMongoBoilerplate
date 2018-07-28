8da14eebb75ce90dc24d893ee6ccc3631b693668
MaciejReimann <maciej.reimann@gmail.com>
2018-07-28 10:02:04 +0200

    mongoose installed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+
 import express from 'express';
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> {
+  console.log(`App is running on port ${PORT}`)
+})
